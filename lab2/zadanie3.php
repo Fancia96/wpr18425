9d281239a6bfabc43e1fae0d688c3beb366c5283
Fancia <as!@as.pl>
2021-04-16 01:47:25 +0200

    zadania2  wrzucam2

--- lab2/zadanie3.php
+++ lab2/zadanie3.php
@@ -1,31 +1,42 @@
+<?php
+if (isset($_POST['osoba'])) {
+    $numberOfPeople = $_POST['osoba'];
+} else {
+    $numberOfPeople = 0;
+}
+?>
 <html>
 <head>
     <meta charset="utf-8">
     <title>kalkulator</title>
 </head>
 <body>
+<?php if (!$numberOfPeople) {?>
 <form method="post">
-    //jeszcze zobaczem
-
-    <label for="ile_osob">Ile osób? </label>
+    <label for="ile_osob">Wybierz dla ilu osób? </label>
     <select name="osoba">
         <option value="1">1</option>
         <option value="2">2</option>
         <option value="3">3</option>
         <option value="4">4</option>
     </select>
+    <button type="submit">Wybierz</button>
+</form>
+    <?php } else {?>
+    <form method="post">
 
-    <p></p>
+        <?php for ($i = 1; $i <= $numberOfPeople; $i++) {?>
+    <p>INFORMACJE DLA OSOBY NUMER <?=$i?> </p>
 
-    <p>Imie    :<input type="text" name="imie"  required></p>
-    <p>Nazwisko:<input type="text" name="nazwisko"  required></p>
-    <p>Adres   :<input type="text" name="adres"></p>
-    <p>Karta   :<input type="text" name="karta"  required></p>
-    <p>Email   :<input type="email" name="email"  required></p>
-    <p>Od dnia :<input type="date" name="od"  required> <label for="dodaj"> format yyyy-mm-dd </label></p>
-    <p>Ile dni :<input type="text" name="dni"></p>
+    <p>Imie    :<input type="text" name="person[<?=$i?>][imie]"  required></p>
+    <p>Nazwisko:<input type="text" name="person[<?=$i?>][nazwisko]"  required></p>
+    <p>Adres   :<input type="text" name="person[<?=$i?>][adres]"></p>
+    <p>Karta   :<input type="text" name="person[<?=$i?>][karta]"  required></p>
+    <p>Email   :<input type="email" name="person[<?=$i?>][email]"  required></p>
+    <p>Od dnia :<input type="date" name="person[<?=$i?>][od]"  required> <label for="dodaj"> format yyyy-mm-dd </label></p>
+    <p>Ile dni :<input type="text" name="person[<?=$i?>][dni]"></p>
     <p>Godzina :
-    <select type="text" name="godzina">
+    <select type="text" name="person[<?=$i?>][godzina]">
         <option value="0">0</option>
         <option value="9:00">9:00</option>
         <option value="12:00">12:00</option>
@@ -35,17 +46,20 @@
     </p>
 
     <label for="dodaj">czy klimatyzacja? </label>
-    <input type="checkbox" name="klimatyzacja">
+    <input type="checkbox" name="person[<?=$i?>][klimatyzacja]">
 
     <label for="dodaj">czy dostawić łózko dla dziecka? </label>
-    <input type="checkbox" name="dziecko">
+    <input type="checkbox" name="person[<?=$i?>][dziecko]">
 
     <label for="dodaj">czy palacz? </label>
-    <input type="checkbox" name="palacz">
+    <input type="checkbox" name="person[<?=$i?>][palacz]">
 
     <p></p>
+        <br><br>
+        <?}?>
     <button type="submit" name="zatwierdz">Zatwierdź</button>
 </form>
+<?}?>
 
 <div id="panel">
 </div>
@@ -53,84 +67,84 @@
 
 <?php
 
-
 if (isset($_POST['zatwierdz'])) {
 
-
-    $doc = new DOMDocument();
-
-    $os = $_POST['osoba'];
-
-    $imie = $_POST['imie'];
-    $nazwisko = $_POST['nazwisko'];
-    $adres = $_POST['adres'];
-    $karta = (int)$_POST['karta'];
-    $email = $_POST['email'];
-    $od = $_POST['od'];
-    $dni = (int)$_POST['dni'];
-    $godzina = $_POST['godzina'];
-
-    $osoba_node = $doc->createElement("h3",
-        'Dane osoby nr ' . $os );
-    $doc->appendChild($osoba_node);
-
-    $osoba_node = $doc->createElement("p",
-        'Imię i Nazwisko: ' . $imie . ' ' . $nazwisko );
-    $doc->appendChild($osoba_node);
-
-    $adres_node = $doc->createElement("p",
-        ' Adres: ' . $adres  );
-    $doc->appendChild($adres_node);
-
-    $karta_node = $doc->createElement("p",
-        ' Nr karty: ' . $karta . ' ' );
-    $doc->appendChild($karta_node);
-
-    $email_node = $doc->createElement("p",
-        ' Email: ' . $email );
-    $doc->appendChild($email_node);
-
-    if (!preg_match("/^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$/",$od)) {
-        echo 'format daty sie nie zgadza';
+    if(empty($_POST['person'])){
         return;
     }
 
-    if(!($dni > 0)){
-        echo 'liczba dni pobytu musi być większa od zera';
-        return;
-    }
+    foreach($_POST['person'] as $person) {
 
-    $dni_node = $doc->createElement("p",
-        ' Pobyt od dnia: ' .
-        $od . ' , zatrzymuje się dni: ' . $dni );
-    $doc->appendChild($dni_node);
+        $doc = new DOMDocument();
 
-    if($godzina != 0) {
-        $godzina_node = $doc->createElement("p",
-            ' Pojwi sie około godziny: ' . $godzina);
-        $doc->appendChild($godzina_node);
-    }
+        $imie = $person['imie'];
+        $nazwisko = $person['nazwisko'];
+        $adres = $person['adres'];
+        $karta = (int)$person['karta'];
+        $email = $person['email'];
+        $od = $person['od'];
+        $dni = (int)$person['dni'];
+        $godzina = $person['godzina'];
 
-    $dodatkoweinformacje = false;
-    $dodatkowe = " Dodatkowo $imie $nazwisko zaznaczył/a że: ";
-    if (array_key_exists('klimatyzacja', $_POST)) {
-        $dodatkoweinformacje = true;
-        $dodatkowe .= " `chcę klimatyzację` ";
-    }
-    if (array_key_exists('dziecko', $_POST)) {
-        $dodatkoweinformacje = true;
-        $dodatkowe .= " `prosi o dostawienie łózka dla dziecka` ";
-    }
-    if (array_key_exists('palacz', $_POST)) {
-        $dodatkoweinformacje = true;
-        $dodatkowe .= " ` jest palaczem` ";
-    }
-    if ($dodatkoweinformacje) {
-        $node4 = $doc->createElement("p", $dodatkowe);
-        $doc->appendChild($node4);
-    }
+        $osoba_node = $doc->createElement("p",
+            'Imię i Nazwisko: ' . $imie . ' ' . $nazwisko);
+        $doc->appendChild($osoba_node);
 
-    echo $doc->saveXML();
+        $adres_node = $doc->createElement("p",
+            ' Adres: ' . $adres);
+        $doc->appendChild($adres_node);
+
+        $karta_node = $doc->createElement("p",
+            ' Nr karty: ' . $karta . ' ');
+        $doc->appendChild($karta_node);
+
+        $email_node = $doc->createElement("p",
+            ' Email: ' . $email);
+        $doc->appendChild($email_node);
+
+        if (!preg_match("/^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$/", $od)) {
+            echo 'format daty sie nie zgadza';
+            return;
+        }
+
+        if (!($dni > 0)) {
+            echo 'liczba dni pobytu musi być większa od zera';
+            return;
+        }
+
+        $dni_node = $doc->createElement("p",
+            ' Pobyt od dnia: ' .
+            $od . ' , zatrzymuje się dni: ' . $dni);
+        $doc->appendChild($dni_node);
+
+        if ($godzina != 0) {
+            $godzina_node = $doc->createElement("p",
+                ' Pojwi sie około godziny: ' . $godzina);
+            $doc->appendChild($godzina_node);
+        }
+
+        $dodatkoweinformacje = false;
+        $dodatkowe = " Dodatkowo $imie $nazwisko zaznaczył/a że: ";
+        if (array_key_exists('klimatyzacja', $person)) {
+            $dodatkoweinformacje = true;
+            $dodatkowe .= " `chcę klimatyzację` ";
+        }
+        if (array_key_exists('dziecko', $person)) {
+            $dodatkoweinformacje = true;
+            $dodatkowe .= " `prosi o dostawienie łózka dla dziecka` ";
+        }
+        if (array_key_exists('palacz', $person)) {
+            $dodatkoweinformacje = true;
+            $dodatkowe .= " ` jest palaczem` ";
+        }
+        if ($dodatkoweinformacje) {
+            $node4 = $doc->createElement("p", $dodatkowe);
+            $doc->appendChild($node4);
+        }
+
+
+        echo $doc->saveXML();
+    }
 }
 
     ?>
